58325948e5fafccbb1aa4e8ea1b3c7890454cccf
adc2108 <alexandraciesielka@gmail.com>
2023-03-04 16:25:52 -0500

    what

--- myapp/app.py
+++ myapp/app.py
@@ -72,7 +72,7 @@ def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
+cap = cv2.VideoCapture(0)
 
 
 if __name__ == '__main__':
