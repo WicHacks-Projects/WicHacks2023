b516d04bb5be54fa5db615cd2d37c8964862c85e
Lindy Quach <lyq2376@rit.edu>
2023-03-04 15:58:32 -0500

    connecting flask

--- myapp/app.py
+++ myapp/app.py
@@ -8,6 +8,59 @@ from pytz import timezone
 
 app = Flask(__name__)
 
+# Define parameters for the hand tracker and gesture recognition
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    max_num_hands=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+# Define parameters for the canvas
+canvas_width = 640
+canvas_height = 480
+canvas = np.zeros((canvas_height, canvas_width, 3), dtype=np.uint8)
+
+# Define function to generate random hand movements
+def generate_hand_movement():
+    # Code to generate random hand movements goes here
+    return hand_movement
+
+# Define route for the main page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Define route for the video feed
+def video_feed():
+    while True:
+        # Get the hand landmarks from the camera
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        # Get the landmarks and draw them on the canvas
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(canvas, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        # Generate a new hand movement and draw it on the canvas
+        hand_movement = generate_hand_movement()
+        # Code to use the hand movement to draw on the canvas goes here
+
+        # Display the canvas
+        ret, buffer = cv2.imencode('.jpg', canvas)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# Define route for the video stream
+@app.route('/video_feed')
+def video_feed():
+    return Response(video_feed(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def home():
