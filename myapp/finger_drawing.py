1416f3ff833ceead1b4c63b00d1d95d8d480d2f5
adc2108 <alexandraciesielka@gmail.com>
2023-03-05 09:08:25 -0500

    SHE ERASES

--- myapp/finger_drawing.py
+++ myapp/finger_drawing.py
@@ -1,102 +1,107 @@
-from tkinter import colorchooser
 import cv2
 import numpy as np
 import mediapipe as mp
-from flask import Flask, Response, render_template
-import time
+import tkinter.colorchooser as colorchooser
 
-app = Flask(__name__)
+# Initialize the video capture object
+cap = cv2.VideoCapture(0)
 
 # Initialize the hand tracking module
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
-
-
-# Get the frame size
-cap = cv2.VideoCapture(0)
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-color = (0, 0, 255)  # Set the default color to red
-
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
 
 # Get the frame size
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# Initialize the drawing canvas and the previous point
+
+# Initialize the drawing canvas, color, and the previous point
 canvas = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
 canvas.fill(0)  # Set the canvas to white
+color = (0, 0, 255)  # Set the default color to red
+prev_x, prev_y = 0, 0
 
-def gen_frames():
-    prev_x, prev_y = 0, 0
-    while True:
-        # Capture a frame from the video feed
-        ret, frame = cap.read()
-
-        # Flip the frame horizontally (mirror effect)
-        frame = cv2.flip(frame, 1)
-
-        # Convert the frame to RGB for hand tracking
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Detect the hand landmarks in the frame
-        results = hands.process(frame_rgb)
-
-        # Draw the landmarks and connect them with lines
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                # Check if the hand is the left or right
-                if hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x < hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].x:
-                    hand = "left"
-                else:
-                    hand = "right"
+drawing = True
+erasing = False
+while True:
+    # Capture a frame from the video feed
+    ret, frame = cap.read()
+    
+    # Flip the frame horizontally (mirror effect)
+    frame = cv2.flip(frame, 1)
+    
+    # Convert the frame to RGB for hand tracking
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # Detect the hand landmarks in the frame
+    results = hands.process(frame_rgb)
+    
+    # Draw the landmarks and connect them with lines
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Check if the hand is the left or right
+            if hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x < hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].x:
+                hand = "left"
+            else:
+                hand = "right"
                 
-                # Draw the landmarks and connect them with lines
-                finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-                cx, cy = int(finger_tip.x * frame_width), int(finger_tip.y * frame_height)
+            # Draw the landmarks and connect them with lines
+            finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            cx, cy = int(finger_tip.x * frame_width), int(finger_tip.y * frame_height)
             
-                # Connect the finger tip with the previous point
-                if prev_x != 0 and prev_y != 0:
+            # Connect the finger tip with the previous point
+            if prev_x != 0 and prev_y != 0 and drawing:
+                if drawing and not erasing:
                     cv2.line(canvas, (prev_x, prev_y), (cx, cy), color, thickness=5)
+                elif erasing:
+                    # Use a white color to simulate erasing
+                    cv2.line(canvas, (prev_x, prev_y), (cx, cy), (0,0, 0), thickness=20)
 
-                #   Update previous point to current point
-                prev_x, prev_y = cx, cy
+            # Update previous point to current point
+            prev_x, prev_y = cx, cy
 
-                # Check if the hand is making a fist
-                if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].x > hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x and hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y:
-                    # Open a color picker when the hand makes a fist
-                    color = colorchooser.askcolor()[0] 
+             # Check if the hand is pinching
+            thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+            thumb_x, thumb_y = int(thumb_tip.x * frame_width), int(thumb_tip.y * frame_height)
+            dist = np.sqrt((cx - thumb_x)**2 + (cy - thumb_y)**2)
+            if dist < 60:
+                drawing = False  # Stop drawing when pinching
+            else:
+                drawing = True  # Resume drawing when not pinching
 
-                else:
-                    color_picker_open = False
-            
-            # Combine the frame and the canvas side by side
-            frame_with_canvas = np.hstack((frame, canvas))
-            
-            # Convert the frame to JPEG
-            ret, buffer = cv2.imencode('.jpg', frame_with_canvas)
-            
-            # Yield the frame as a JPEG image
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            
-            # Clear the canvas when a key is pressed
-            if cv2.waitKey(1) == ord('c'):
-                canvas.fill(0)  # Clear the canvas to black
-                prev_x, prev_y = 0, 0  # Reset the previous point
-            if cv2.waitKey(1) == ord('q'):
-                break
+            # Check if the hand is making a fist
+            if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].x > hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x and hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y:
+                # Open a color picker when the hand makes a fist
+                color = colorchooser.askcolor()[0] 
+            else:
+                color_picker_open = False
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+            # Check if the thumb is down for erasing
+            # Check if the hand is making a thumbs down gesture
+            if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and \
+                hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y and \
+                hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y:
+                erasing = False
+            else:
+                erasing = True
+    
+    # Show the canvas in a separate window
+    cv2.imshow('Canvas', canvas)
+    
+    # Combine the frame and the canvas
+    frame_with_canvas = cv2.add(frame, canvas)
+    
+    # Show the webcam feed in a separate window
+    cv2.imshow('Webcam', frame_with_canvas)
+    
+    # Quit the program if 'q' is pressed
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-@app.route('/draw')
-def draw():
-    return Response(gen_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    # Clear the canvas and video feed when 'c' is pressed
+    if cv2.waitKey(1) == ord('c'):
+        canvas.fill(0)  # Clear the canvas to black
+        prev_x, prev_y = 0, 0  # Reset the previous point
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+# Release the resources
+cap.release()
+cv2.destroyAllWindows()
